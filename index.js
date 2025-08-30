f3a0de3df21bd6eee79b4a3855209ad585cd06b7
Pravek Khata <pravek.khata01@gmail.com>
2025-08-30 16:03:11 +0530

    Remove eval function and perform calculations from scratch

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
 const display = document.getElementById("display");
 
 function appendToDisplay(input){
-    display.value += input;
+    display.value += input + " ";
 }
 
 function clearDisplay(){
     display.value = "";
 }
 
+function operate(num1, operator, num2) {
+    switch(operator){
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case 'x':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        case '%':
+            return remainder(num1, num2);
+        default:
+            return "Invalid operator";
+    }
+}
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    console.log(num1-num2)
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function remainder(num1, num2){
+    return num1 % num2;
+}
+
 function calculate(){
-    display.value = eval(display.value);
+    const arrOfInputs = display.value.trim().split(" ");
+    console.log(arrOfInputs);
+    const num1 = parseInt(arrOfInputs[0]);
+    const operator = arrOfInputs[1];
+    const num2 = parseInt(arrOfInputs[2]);
+
+    console.log(typeof num1, typeof num2, typeof operator);
+    const result = operate(num1, operator, num2);
+    console.log(result);
+    display.value = result + " ";
 }
 
 function clearLastCharacter(){
